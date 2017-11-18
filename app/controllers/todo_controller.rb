87b846156aeadd127e53aedeabcd01ddaea45e9b
WenXing Cai <enwayschool@gmail.com>
2017-11-18 22:26:29 +0000

    Using Arrays to show todos and times

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -6,6 +6,6 @@ class TodoController<ApplicationController
     def show
         @todo_description = ["AP Physics Homework","English Homework","Make the curriculum","Laundry","Going to Hawaii"]
         @todo_minutes_estimate = ["60","45","60","50","47520"]
-        :id.to_s
+        @todo_index = params[:id].to_i
     end
 end   
