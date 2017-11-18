ad650505fc9cf1435d8aef742c836e86b0d798a2
WenXing Cai <enwayschool@gmail.com>
2017-11-18 21:58:24 +0000

    added ruby to page

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,8 +1,11 @@
 class TodoController<ApplicationController
     def index
-        
+        @todo_description = ["AP Physics Homework","English Homework","Make the curriculum","Laundry","Going to Hawaii"]
+        @todo_minutes_estimate = ["60","45","60","50","47520"]
     end              
     def show
-        
+        @todo_description = ["AP Physics Homework","English Homework","Make the curriculum","Laundry","Going to Hawaii"]
+        @todo_minutes_estimate = ["60","45","60","50","47520"]
+        :id.to_s
     end
 end   
