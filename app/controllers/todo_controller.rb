847edc72811c46c91afa1863836fc7b7ca86cf00
WenXing Cai <enwayschool@gmail.com>
2017-11-19 23:05:30 +0000

    Redid index page with new changes

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,9 +1,13 @@
 class TodoController<ApplicationController
     def index
-        @todo_description = ["AP Physics Homework","English Homework","Make the curriculum","Laundry","Going to Hawaii"]
-        @todo_minutes_estimate = ["60","45","60","50","10080"]
+        @todos = Todo.all
     end              
     def new
+    end
+    def destroy
+        t = Todo.find_by_id(params[:id])
+        t.destroy
+        redirect_to "/"
     end
     def edit
         @todo = Todo.find_by_id(params[:id])
@@ -21,6 +25,13 @@ class TodoController<ApplicationController
         t.pomodoro_estimate = params['pomodoro_estimate']
         t.save
         redirect_to "/todo/show/#{ t.id }"
+    end
+    def createi
+        t = Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/"
     end
     def show
         #@todo_description = ["AP Physics Homework","English Homework","Make the curriculum","Laundry","Going to Hawaii"]
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'todo/index',to:'todo#index'
+  root to: 'todo#index'
   get 'todo/new', to: 'todo#new'
   get 'todo/create', to: 'todo#create'
+  get 'todo/createi', to: 'todo#createi'
   get 'todo/edit/:id', to: 'todo#edit'
   get 'todo/update/:id', to: 'todo#update'
+  get 'todo/delete/:id', to: 'todo#destroy'
   get 'todo/show/:id',to:'todo#show'
 end
