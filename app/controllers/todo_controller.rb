c71676b02620f3e333b9f567cddf5135a4743fde
WenXing Cai <enwayschool@gmail.com>
2017-11-19 18:39:45 +0000

    Replaced conditional logic with the database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,11 +1,13 @@
 class TodoController<ApplicationController
     def index
-        @todo_description = ["AP Physics Homework","English Homework","Make the curriculum","Laundry","Going to Hawaii"]
-        @todo_minutes_estimate = ["60","45","60","50","10080"]
+        #@todo_description = ["AP Physics Homework","English Homework","Make the curriculum","Laundry","Going to Hawaii"]
+        #@todo_minutes_estimate = ["60","45","60","50","10080"]
     end              
     def show
-        @todo_description = ["AP Physics Homework","English Homework","Make the curriculum","Laundry","Going to Hawaii"]
-        @todo_minutes_estimate = ["60","45","60","50","10080"]
-        @todo_index = params[:id].to_i
+        #@todo_description = ["AP Physics Homework","English Homework","Make the curriculum","Laundry","Going to Hawaii"]
+        #@todo_minutes_estimate = ["60","45","60","50","10080"]
+        #@todo_index = params[:id].to_i
+        @todo = Todo.find_by_id(params[:id])
+        
     end
 end   
