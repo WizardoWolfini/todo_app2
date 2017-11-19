c72df59dfa00bc027072d684ab531907d628129e
WenXing Cai <enwayschool@gmail.com>
2017-11-19 19:36:02 +0000

    added user input to make new todos using a create page

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -8,6 +8,13 @@ class TodoController<ApplicationController
         
         
     end
+    def create
+        t = Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
     def show
         #@todo_description = ["AP Physics Homework","English Homework","Make the curriculum","Laundry","Going to Hawaii"]
         #@todo_minutes_estimate = ["60","45","60","50","10080"]
